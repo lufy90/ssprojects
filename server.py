af2fd945bb7330c77d70f8c3533eafa0523e3e1c
lufei <very90@foxmail.com>
2019-04-12 19:55:05 +0800

    Add push list stat and logout functions, basically work well.

--- server.py
+++ server.py
@@ -4,12 +4,13 @@
 
 
 import socket, json, socketserver
-import sys
+import sys, os
+import pickle
 
+from lib import bytes_to_obj, obj_to_bytes, md5sum
 from settings import *
+from user import User
 
-
-BUF_SIZE=1024
 
 
 class Server(socketserver.TCPServer, socketserver.ForkingMixIn):
@@ -22,7 +23,7 @@ class LoginServerHandler(socketserver.BaseRequestHandler):
     data = RequestData(recv_data, self)
     data.response()
     '''
-    data = byte_to_obj(recv_data)
+    data = bytes_to_obj(recv_data)
     if auth(data):
       self.request.sendall(b'Login OK.')
     else:
@@ -31,19 +32,16 @@ class LoginServerHandler(socketserver.BaseRequestHandler):
 
 class FileTransferHandler(socketserver.StreamRequestHandler):
   def handle(self):
+    self.data = self.rfile.readline().strip()
+    
     pass
-
-
-def byte_to_obj(byte_data):
-  return json.loads(byte_data.decode('utf-8'))
 
 def auth(data):
   expect_username = 'test'
   expect_password = 'abc123'
   try:
-    if data['request_type'] == 'login':
-      recv_username = data['username']
-      recv_pasword = data['userkey']
+    recv_username = data['username']
+    recv_pasword = data['userkey']
     if (recv_username, recv_pasword) == (expect_username, expect_password):
       return True
     else:
@@ -76,33 +74,131 @@ def run():
 class RequestData():
   ''' resolve incoming data and make response.'''
   def __init__(self, byte_data, handler):
-    self.data = byte_to_obj(byte_data)
+    self.data = bytes_to_obj(byte_data)
+    self.user = User(self.data['username'])
     self.handler = handler
     self.call_response = {
       'login': self.login,
       'push': self.push,
+      'list': self.listfiles,
+      'logout': self.logout,
+      'stat': self.status,
     }
 
   def response(self):
     print('recv: ',self.data)
-    try:
+    if data_valid(self.data) and auth(self.data):
       self.call_response[self.data['request_type']]()
-    except KeyError as e:
-      print('Caution: received invalid data.')
+    else:
+      code, key = 1, None
+      sendbackdata = {'code':code, 'userkey': key}
+      self.handler.request.sendall(obj_to_bytes(sendbackdata))
 
   def login(self):
-    recv_username = self.data['username']
-    if data_valid(self.data) and auth(self.data):
-      self.handler.request.sendall(b'Login OK')
-      self.handler.request.sendall(b'Ready for recv data')
-    else: 
-      self.handler.request.sendall(b'Auth Failed')
+    code, key = 0, key_gen()
+    sendbackdata = {'code':code, 'content': key}
+    self.handler.request.sendall(obj_to_bytes(sendbackdata))
 
-  def push(self, data):
-    pass
+  def push(self):
+    code = 0
+    sendbackdata = {'code':code}
+    try:
+      dir_name = self.data['content'][0]
+      file_name = self.data['content'][1]
+      file_md5sum = self.data['content'][2]
+    except IndexError as e:
+      print(e)
+      print('Reseive bad push data')
+      sys.exit(4)
+    data_dir = self.user.root_dir + '/' + dir_name
+    cache_dir = data_dir + '/' + S_CACHE_DIR
 
-def key_gen(time, user, client,  ):
-  pass
+    if not os.path.exists(data_dir):
+      os.mkdir(data_dir)
+      os.mkdir(cache_dir)   # cache file.
+      with open(cache_dir + '/' + 'meta', 'wb') as f:
+        pickle.dump({}, f)
+
+    full_file_name = data_dir + '/' + file_name
+    if not file_md5sum:    # file_md5sum = '', get a directory.
+      self.handler.request.sendall(obj_to_bytes(sendbackdata))
+      print('Get dir: %s' % file_name)
+      try:
+        os.mkdir(full_file_name)
+        print('Transfer OK: ', file_name)
+      except Exception as e:
+        print(e)
+        print('Transfer failed: ', file_name)
+    else:
+      print('Get file: %s' % file_name)
+      try:
+        transfer_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transfer_sock.bind((DEFAULT_HOST, DEFAULT_TRANSFER_PORT))
+        transfer_sock.listen(1)
+        self.handler.request.sendall(obj_to_bytes(sendbackdata))
+        conn, addr = transfer_sock.accept()
+      except socket.error as e:
+        print(e)
+      with open(full_file_name, 'wb') as f:
+        while True:
+          trsf_data = conn.recv(BUF_SIZE)
+          if not trsf_data:
+            break
+          f.write(trsf_data)
+        conn.close()
+        print('file wrote: ', full_file_name)
+      transfer_sock.close()
+
+    try:
+      if md5sum(full_file_name) == file_md5sum:
+        print('Transfer OK: ', file_name)
+        # sendback OK.
+        with open(cache_dir + '/' + 'meta', 'rb') as f:
+          file_meta = pickle.load(f)
+
+        file_meta[file_name] = file_md5sum
+
+        with open(cache_dir + '/' + 'meta', 'wb') as f:
+          pickle.dump(file_meta, f)
+      else:
+        print('Transfer failed: ', file_name)
+    except os.error as e:
+      print('Transfer failed: ', file_name)
+      print(e)
+        
+  def listfiles(self):
+    code = 0
+    dir_name = self.data['content'][0]
+
+    full_dir_name = self.user.root_dir + '/' + dir_name
+    try:
+      send_back_content = os.listdir(full_dir_name)
+    except os.error as e:
+      code = 1
+      send_back_content = [str(e)]
+     
+    sendbackdata = {'code':code, 'content': send_back_content}
+    self.handler.request.sendall(obj_to_bytes(sendbackdata))
+
+  def logout(self):
+    code = 0
+    send_back_content = None
+    sendbackdata = {'code':code, 'content': send_back_content}
+    self.handler.request.sendall(obj_to_bytes(sendbackdata))
+
+  def status(self):
+    code = 0
+    dir_name = self.data['content'][0]
+    data_dir = self.user.root_dir + '/' + dir_name
+    cache_dir = data_dir + '/' + S_CACHE_DIR
+    with open(cache_dir + '/' + 'meta', 'rb') as f:
+      file_meta = pickle.load(f)
+    sendbackdata = {'code':code, 'content':file_meta}
+    self.handler.request.sendall(obj_to_bytes(sendbackdata))
+
+
+def key_gen():
+  return 'abc123'
 
 
 if __name__ == '__main__':
@@ -113,5 +209,9 @@ if __name__ == '__main__':
   given_args = parser.parse_args()
   HOST = (given_args.host or 'localhost')
   PORT = (given_args.port or DEFAULT_PORT)
+  try:
+    run()
 
-  run()
+  except Exception as e:
+    print(e)
+    sys.exit(0)
