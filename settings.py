935d6c927e4d3c6e5b230c49b05738fdb089620b
lufei <very90@foxmail.com>
2019-04-12 19:56:33 +0800

    Transfer settings like DEFAULT_TRANSFER_PORT and others

--- settings.py
+++ settings.py
@@ -3,15 +3,22 @@
 # 20190329
 # ssgit settings.
 
+# Common
+
 DEFAULT_HOST='localhost'
 DEFAULT_USER=''
 DEFAULT_PORT=8888
 DEFAULT_ONLINE_TIME=3600
+DEFAULT_TRANSFER_PORT=8889
+BUF_SIZE=1024
 
+
+FSIZE_MAX=1024
+DSIZE_MAX=4096
 
 # For client only
 LOGIN_TIMEOUT=10
-
+C_CACHE_DIR='./.c_cache'
 
 
 # For server only
@@ -22,6 +29,8 @@ DATA_STRUCT = {
             ('request_type', str, 16, False),
             ('username', str, 16, False),
             ('userkey', str, 128, False),
-            ('content', dict, 1024, True),
+            ('content', list, 1024, True),
            }
 
+DATA_DIR = '/tmp/ssgit'
+S_CACHE_DIR='.cache'
