5a3851958e22ecc9b4d646a4bbbc6d2f2044cbca
lufei <very90@foxmail.com>
2019-04-12 19:58:01 +0800

    Add basic structure for user(s)

--- user.py
+++ user.py
@@ -2,14 +2,19 @@
 # user.py
 # 20190403
 
+from settings import DATA_DIR 
+import os
+from database import Sqlite3
 
 class User():
   def __init__(self, name):
     self.name = name
+    self.root_dir = DATA_DIR + '/' + self.name
 
   def create(self, password):
-    pass
+    os.mkdir(self.root)
 
   def auth(self, data):
     pass
+
  
